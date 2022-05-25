63ad4a89628bd5833b7c6e70ebe8ef19b28542e0
Rayvivek881 <Rayvivek881@gmail.com>
2022-05-25 00:29:04 -0700

    nothing

---
+++ src/keys.js
@@ -0,0 +1,6 @@
+module.exports = {
+    MONGOURI: 'mongodb+srv://vivek:Jh3OXa8qPc0thCNN@nodetuts.6d6nn.mongodb.net/Database?retryWrites=true&w=majority',
+    user: 'rayvivek881@gmail.com',
+    pass: '1221344V',
+    skey: 98,
+}
