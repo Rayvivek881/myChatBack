1f1f611fe6ef9809eaae6b9ba3c6093115cc847d
Rayvivek881 <Rayvivek881@gmail.com>
2022-05-25 01:33:36 -0700

    nothing

--- src/app.js
+++ src/app.js
@@ -19,12 +19,12 @@ mongoose.connect(MONGOURI, { useNewUrlParser: true, useUnifiedTopology: true })
 app.use(express.json({limit: "50mb"}));
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
+app.use(cors());
 
 app.use(routerLS);
 app.use(routerFriend);
 app.use(routersearch); 
 app.use(routerPost);
 app.use(routerGC);
-app.use(cors());
 
 app.listen(port, () => { console.log(`we are working port ${port}`); });
